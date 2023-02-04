8fe687e1f061bf71433c4226214c997893b8749f
Nyk <thenykreed@gmail.com>
2023-02-04 17:01:28 -0600

    many things

--- scenes/gameplay/diggable/diggable.gd
+++ scenes/gameplay/diggable/diggable.gd
@@ -1,8 +1,33 @@
 extends Node3D
 
 
+var nose: Node3D = null
+
+@onready var dig_sprite: Sprite2D = $SubViewport/Sprite2D as Sprite2D
 @onready var viewport: SubViewport = $SubViewport as SubViewport
 
 
-func _process(delta: float) -> void:
+func _ready() -> void:
 	viewport.render_target_clear_mode = SubViewport.CLEAR_MODE_NEVER
+
+
+func _physics_process(delta: float) -> void:
+	if nose:
+		var local_pos: Vector3 = to_local(nose.global_transform.origin)
+		var pos2d: Vector2 = Vector2(local_pos.x, local_pos.z)
+		pos2d.x = remap(pos2d.x, -0.5, 0.5, 0, 128)
+		pos2d.y = remap(pos2d.y, -0.5, 0.5, 0, 128)
+		dig_sprite.position = pos2d
+		dig_sprite.self_modulate.a = clamp(1.0 - local_pos.z, 0, 1)
+	else:
+		dig_sprite.self_modulate.a = 0.0
+
+
+func _on_area_3d_area_entered(area: Area3D) -> void:
+	if not nose:
+		nose = area
+
+
+func _on_area_3d_area_exited(area: Area3D) -> void:
+	if nose == area:
+		nose = null
--- scenes/gameplay/truffler/truffler.gd
+++ scenes/gameplay/truffler/truffler.gd
@@ -3,7 +3,7 @@ extends CharacterBody3D
 
 const SPEED: float = 5.0
 const JUMP_VELOCITY: float = 4.5
-const LOOK_CONSTRAINT: float = 40.0
+const LOOK_CONSTRAINT: float = 60.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
