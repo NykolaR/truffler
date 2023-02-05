83fc8c48aa86aa414dc19f77a6de9e643a2b5f5c
Nyk <thenykreed@gmail.com>
2023-02-05 11:32:18 -0600

    huetno

--- scenes/gameplay/truffler/truffler.gd
+++ scenes/gameplay/truffler/truffler.gd
@@ -16,15 +16,26 @@ var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var sniff_player: AudioStreamPlayer = $Sniff as AudioStreamPlayer
 @onready var walkies: AudioStreamPlayer = $Walkies as AudioStreamPlayer
 
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+
 func _input(event: InputEvent) -> void:
 	if event.is_action("oink"):
 		oinks[randi()%oinks.size()].play()
+	
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x / 600.0)
+		camera.rotate_x(-event.relative.y / 600.0)
+		camera.rotation_degrees.x = clamp(camera.rotation_degrees.x, -LOOK_CONSTRAINT_DOWN, LOOK_CONSTRAINT_UP)
 
 
 func _physics_process(delta: float):
 	_camera(delta)
 	_movement(delta)
-	walkies.volume_db = clamp(linear_to_db(clamp(velocity.length_squared()/3.0, 0.0, 1.0)), -80.0, -3.0)
+	var movement: Vector2 = Vector2(velocity.x, velocity.z)
+	walkies.volume_db = clamp(linear_to_db(clamp(movement.length_squared()/3.0, 0.0, 1.0)), -80.0, -3.0)
 	move_and_slide()
 	
 	if not smellovision.emitting == Input.is_action_pressed("smelly"):
@@ -37,9 +48,7 @@ func _physics_process(delta: float):
 
 func _camera(delta: float) -> void:
 	var input_dir: Vector2 = Input.get_vector("look_left", "look_right", "look_up", "look_down")
-	# if input type is mouse, get mouse input and overwrite
 	var intensity: float = clamp(input_dir.length(), 0, 1)
-	
 	rotate_y(-input_dir.x * delta * sense)
 	camera.rotate_x(-input_dir.y * delta * sense)
 	camera.rotation_degrees.x = clamp(camera.rotation_degrees.x, -LOOK_CONSTRAINT_DOWN, LOOK_CONSTRAINT_UP)
@@ -62,6 +71,9 @@ func _movement(delta: float) -> void:
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+		if Input.is_action_pressed("sprint"):
+			velocity.x *= 5.0
+			velocity.z *= 5.0
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
