c43726f2b50979fc21b9278da01ee58aadc30c8f
Nyk <thenykreed@gmail.com>
2023-02-05 14:01:39 -0600

    SCARY

--- scenes/gameplay/tower/tower.gd
+++ scenes/gameplay/tower/tower.gd
@@ -6,9 +6,11 @@ func _on_area_3d_body_entered(body: Node3D) -> void:
 	$AudioStreamPlayer3D.play()
 	var tween: Tween = get_tree().create_tween()
 	tween.tween_property(light, "color", Color.RED, 1.0)
+	tween.tween_property($AnimationPlayer, "speed_scale", 0.0, 1.0)
 
 
 func _on_area_3d_body_exited(body: Node3D) -> void:
 	$AudioStreamPlayer3D.play()
 	var tween: Tween = get_tree().create_tween()
 	tween.tween_property(light, "color", Color(0.98431372642517, 0.86666667461395, 0.76078432798386), 1.0)
+	tween.tween_property($AnimationPlayer, "speed_scale", 0.5, 1.0)
